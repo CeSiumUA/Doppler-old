f5f1b9a9ef629efc6086b253a38712c71d894263
CeSiumUA <46378315+CeSiumUA@users.noreply.github.com>
2020-05-27 19:57:17 +0300

    tiny commit :)

---
+++ Doppler/Doppler.iOS/DependencyInjections/FireBaseConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Doppler.DependencyInjections;
+using Doppler.iOS.DependencyInjections;
+
+[assembly: Xamarin.Forms.Dependency(typeof(GooglePlayServicesAvailablility))]
+namespace Doppler.iOS.DependencyInjections
+{
+    public class FireBaseConnector:IFireBaseComponent
+    {
+        public FireBaseConnector()
+        {
+        }
+        //TODO
+        public bool CheckGooglePlayServicesAvailability()
+        {
+            return true;
+        }
+
+        public string GetFireBaseToken()
+        {
+            
+        }
+    }
+}
