d80e90157555e3d690faae2e991986109913fa6e
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-02-27 17:29:30 +0200

    Added cryptography tests

---
+++ Doppler/Doppler.Tests/AuthenticationServicesTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Doppler.API.Social;
+using Doppler.REST.Models.Authentication;
+using Doppler.REST.Models.Cryptography;
+using Doppler.REST.Models.Repository;
+using Doppler.REST.Models.Social;
+using Doppler.REST.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Doppler.Tests
+{
+    [TestClass]
+    public class AuthenticationServicesTest
+    {
+        private IRepository repository;
+        private ICryptographyProvider cryptographyProvider;
+        [TestInitialize]
+        public void Init()
+        {
+            var repoMock = new Mock<IRepository>();
+            repoMock.Setup(repository => repository.GetDopplerUserWithPassword("fedir").Result)
+                .Returns(new DopplerUser()
+                {
+                    Email = "fedir@dot.com",
+                    Id = 1,
+                    Login = "fedir",
+                    Password = new Password()
+                    {
+                        PasswordHash = "12345"
+                    }
+                });
+            this.repository = repoMock.Object;
+            var cryptoMock = new Mock<ICryptographyProvider>();
+        }
+        [TestMethod]
+        [DataRow("fedir", "12345")]
+        public void AuthenticationServiceLogin_Test(string login, string password)
+        {
+            
+        }
+    }
+}
---
+++ Doppler/Doppler.Tests/Cryptography/CryptographyHashTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Doppler.REST.Models.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Doppler.Tests.Cryptography
+{
+    [TestClass]
+    public class CryptographyHashTests
+    {
+        [TestMethod]
+        [DataRow("12345Aa-")]
+        public void PasswordCorrectHashCheck_Test(string password)
+        {
+            var pw = HashProvider.GenerateHash(password);
+            var passwordMatch = HashProvider.CompareHash(password, pw);
+            Assert.IsTrue(passwordMatch);
+        }
+        [TestMethod]
+        [DataRow("12345Aa-", "12345Aa")]
+        public void PasswordInCorrectHashCheck_Test(string originPassword, string inputPassword)
+        {
+            var pw = HashProvider.GenerateHash(originPassword);
+            var passwordMatch = HashProvider.CompareHash(inputPassword, pw);
+            Assert.IsFalse(passwordMatch);
+        }
+    }
+}
