bfdb98187337ad6e4da6517d587698bbffcb53e5
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-03-13 10:42:55 +0200

    Added Social Migration

--- Doppler/Doppler.REST/Models/Repository/ApplicationDatabaseContext.cs
+++ Doppler/Doppler.REST/Models/Repository/ApplicationDatabaseContext.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Doppler.API.Authentication;
 using Doppler.API.Social;
+using Doppler.API.Social.Chatting;
 using Doppler.API.Storage;
+using Doppler.API.Storage.FileStorage;
 using Doppler.REST.Models.Authentication;
 using Doppler.REST.Models.Cryptography;
 using Doppler.REST.Models.Social;
@@ -22,6 +24,19 @@ namespace Doppler.REST.Models
         public DbSet<JwtToken> RefreshTokens { get; set; }
         public DbSet<UserContact> UsersContacts { get; set; }
         #endregion
+
+        #region FileStorage
+        public DbSet<BLOB> Blobs { get; set; }
+        public DbSet<Data> Files { get; set; }
+        #endregion
+        #region SocialDbSet
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<ConversationMember> ConversationMembers { get; set; }
+        public DbSet<ConversationMessage> ConversationMessages { get; set; }
+        public DbSet<ConversationMessageContent> ConversationMessageContents { get; set; }
+        public DbSet<ConversationMessageMediaContent> ConversationMessageMediaContents { get; set; }
+        public DbSet<ConversationMessageViewer> ConversationMessageViewers { get; set; }
+        #endregion
         private readonly IConfiguration configuration;
         public ApplicationDatabaseContext(IConfiguration configuration)
         {
@@ -37,6 +52,7 @@ namespace Doppler.REST.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasMany(user => user.UserContacts).WithOne(user => user.User);
+            modelBuilder.Entity<ConversationMessage>().HasOne(message => message.Content).WithOne(content => content.Message).HasForeignKey<ConversationMessageContent>(x => x.Id);
             base.OnModelCreating(modelBuilder);
         }
     }
