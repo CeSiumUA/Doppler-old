69a36e767ee9222c5bea44a724e8e0d53199c507
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-03-16 20:50:00 +0200

    implemented login page

--- Doppler/Doppler.API/Social/User.cs
+++ Doppler/Doppler.API/Social/User.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using System.Text.Json.Serialization;
 using Doppler.API.Authentication;
+using Doppler.API.Storage.FileStorage;
 
 namespace Doppler.API.Social
 {
@@ -16,7 +18,18 @@ namespace Doppler.API.Social
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Login { get; set; }
+        [JsonIgnore]
         public List<UserContact> UserContacts { get; set; }
+        [JsonIgnore]
+        public Data Icon { get; set; }
+        [NotMapped]
+        public string IconUrl
+        {
+            get
+            {
+                return Icon?.Id.ToString();
+            }
+        }
         public SignedInUser GetSignedInUser(JwtToken accessJwtToken, JwtToken refreshToken = null)
         {
             return new SignedInUser()
