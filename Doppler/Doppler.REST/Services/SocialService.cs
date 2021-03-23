682e28e0b502d8dbceddc5b4cdde2ad6f6f739df
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-03-23 10:31:05 +0200

    Implemented basic social service

---
+++ Doppler/Doppler.REST/Services/SocialService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doppler.REST.Models.Repository;
+
+namespace Doppler.REST.Services
+{
+    public class SocialService
+    {
+        private readonly IRepository repository;
+        private readonly IdentityService identityService;
+        public SocialService(IRepository repository, IdentityService identityService)
+        {
+            this.repository = repository;
+            this.identityService = identityService;
+        }
+    }
+}
